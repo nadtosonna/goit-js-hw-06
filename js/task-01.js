370f066235fc34a10b55d16d2bfea582a5453c0b
Kateryna Prasolova <prasolova.kateryna@gmail.com>
2022-06-27 21:09:18 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -3,6 +3,6 @@ const categoryList = document.querySelectorAll('.item');
 console.log(`Number of categories: ${categoryList.length}`);
 
 const result = [...categoryList].map(element =>
-    `Category: ${element.firstElementChild.textContent} ${'\n'}Elements: ${element.children[1].children.length} ${'\n'}`).join("\n");
+    `Category: ${element.firstElementChild.textContent} ${'\n'}Elements: ${element.lastElementChild.children.length} ${'\n'}`).join("\n");
 
 console.log(result);
