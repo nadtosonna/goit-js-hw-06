4f3be11d7b972ad4911ec0658fd74eb5c81eab90
Kateryna Prasolova <prasolova.kateryna@gmail.com>
2022-06-27 23:58:18 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -8,10 +8,11 @@ const ingredients = [
 ];
 const ingredientList = document.getElementById('ingredients');
 ingredientList.textContent = 'INGREDIENTS:';
+ingredientList.setAttribute("style", "font-weight: 700;");
 
 const listItems = ingredients.forEach(element => {
   const products = document.createElement('li');
   products.textContent = `${element}`;
   products.classList.add("item");
-  ingredientList.append(products);
+  ingredientList.appendChild(products);
 });
