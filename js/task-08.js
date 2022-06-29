5baecc03439e6924a00d9093854ea8eb7e1fe2f3
Kateryna Prasolova <prasolova.kateryna@gmail.com>
2022-06-30 01:16:57 +0300

    task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formDataElements = event.currentTarget.elements;
+  if (formDataElements.email.value === "" || formDataElements.password.value === "") {
+    alert("Please fill in all the fields!");
+  } else {
+    console.log(`Login: ${formDataElements.email.value}, Password: ${formDataElements.password.value}`);
+    event.currentTarget.reset();
+  }
+}
