c86a4fa868aa3ac6b5ec73db2af85f4211b01b55
Kateryna Prasolova <prasolova.kateryna@gmail.com>
2022-06-30 11:23:56 +0300

    task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const btnColorChange = document.querySelector(".change-color");
+const textColorOutput = document.querySelector(".color");
+
+btnColorChange.addEventListener("click", changeBackgroundColor);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function changeBackgroundColor() {
+  let color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  textColorOutput.textContent = color;
+}
+
